8c98913d57d6bc8c42017e527144e1b7d0945254
rchaser53 <tayoshizawa29@gmail.com>
2019-05-07 20:30:41 +0900

    try to calculation like a + b * c

--- src/expr.rs
+++ src/expr.rs
@@ -1,7 +1,8 @@
 use combine::error::ParseError;
 use combine::parser::char::{char, digit, letter, spaces};
 use combine::stream::Stream;
-use combine::{between, choice, many1, parser, sep_by, Parser};
+use combine::{between, choice, many, many1, parser, sep_by, Parser};
+use std::mem;
 
 pub mod bin_op;
 use bin_op::{bin_op as bin_op_, BinOpKind};
@@ -12,7 +13,7 @@ use uni::{integer, uni as create_uni, Uni};
 #[derive(Debug, PartialEq)]
 pub enum Expr {
     Unary(Uni),
-    Binary(Uni, BinOpKind, Uni),
+    Binary(Box<Expr>, BinOpKind, Box<Expr>),
 }
 
 pub fn expr_<I>() -> impl Parser<Input = I, Output = Expr>
@@ -20,8 +21,9 @@ where
     I: Stream<Item = char>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    let skip_spaces = || spaces().silent();
-    choice((binary(), unary())).skip(skip_spaces())
+    // let skip_spaces = || spaces().silent();
+    // choice((binary(), unary())).skip(skip_spaces())
+    binary()
 }
 
 pub fn unary<I>() -> impl Parser<Input = I, Output = Expr>
@@ -37,13 +39,24 @@ where
     I: Stream<Item = char>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    integer()
+    unary()
         .skip(spaces())
-        .and(bin_op_())
+        .and(many(bin_op_().skip(spaces()).and(unary())))
         .skip(spaces())
-        .and(integer())
-        .skip(spaces())
-        .map(|((left, op), right)| Expr::Binary(left, op, right))
+        .map(|(left, mut others): (Expr, Vec<(BinOpKind, Expr)>)| {
+            let length = others.len();
+            if length == 0 {
+                return left;
+            }
+
+            let mut exp = left;
+            for _ in 0..length {
+                let (bin_op, right) = others.swap_remove(0);
+                exp = Expr::Binary(Box::new(exp), bin_op, Box::new(right));
+            }
+
+            exp
+        })
 }
 
 parser! {
@@ -64,6 +77,40 @@ mod test {
         assert_eq!(
             expr().easy_parse(r#""abc""#),
             Ok((Expr::Unary(Uni::String(String::from("abc"))), ""))
+        );
+    }
+
+    #[test]
+    fn add_test() {
+        assert_eq!(
+            expr().easy_parse(r#"1 + 2"#),
+            Ok((
+                Expr::Binary(
+                    Box::new(Expr::Unary(Uni::Number(1))),
+                    BinOpKind::Add,
+                    Box::new(Expr::Unary(Uni::Number(2))),
+                ),
+                ""
+            ))
+        );
+    }
+
+    #[test]
+    fn add_and_() {
+        assert_eq!(
+            expr().easy_parse(r#"1 + 2 * 3"#),
+            Ok((
+                Expr::Binary(
+                    Box::new(Expr::Binary(
+                        Box::new(Expr::Unary(Uni::Number(1))),
+                        BinOpKind::Add,
+                        Box::new(Expr::Unary(Uni::Number(2)))
+                    )),
+                    BinOpKind::Mul,
+                    Box::new(Expr::Unary(Uni::Number(3))),
+                ),
+                ""
+            ))
         );
     }
 }
