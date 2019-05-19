1f5feab2898bb0e56130584f39f77c40c167cb34
rchaser53 <tayoshizawa29@gmail.com>
2019-05-19 22:02:28 +0900

    add field test to Uni and fix bug

--- src/expr/uni.rs
+++ src/expr/uni.rs
@@ -1,7 +1,7 @@
 use combine::error::ParseError;
 use combine::parser::char::{char, digit, letter, spaces};
 use combine::stream::Stream;
-use combine::{between, choice, many1, parser, sep_by, token, Parser};
+use combine::{attempt, between, choice, many1, parser, sep_by, sep_by1, token, Parser};
 
 #[derive(Debug, PartialEq)]
 pub struct Id(pub String);
@@ -13,7 +13,7 @@ pub enum Uni {
     String(String),
     Number(i32),
     Boolean(Boolean),
-    Field(Vec<Box<Uni>>),
+    Field(Vec<Id>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -28,7 +28,8 @@ where
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let skip_spaces = || spaces().silent();
-    choice((word_(), array(), string(), integer())).skip(skip_spaces())
+
+    choice((attempt(field()).or(word_()), array(), string(), integer())).skip(skip_spaces())
 }
 
 pub fn field<I>() -> impl Parser<Input = I, Output = Uni>
@@ -37,17 +38,22 @@ where
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let skip_spaces = || spaces().silent();
-    sep_by(word(), token('.'))
+    sep_by1(word(), token('.'))
         .skip(skip_spaces())
-        .map(|words: Vec<Uni>| {
-            let fields = words
-                .into_iter()
-                .map(|word| match word {
-                    Uni::Id(_) => Box::new(word),
-                    _ => panic!("should come here Id. but actual: {:?}", word),
-                })
-                .collect();
-            Uni::Field(fields)
+        .map(|mut words: Vec<Uni>| {
+            let length = words.len();
+            if length > 1 {
+                let fields = words
+                    .into_iter()
+                    .map(|word| match word {
+                        Uni::Id(id) => id,
+                        _ => panic!("should come here Id. but actual: {:?}", word),
+                    })
+                    .collect();
+                Uni::Field(fields)
+            } else {
+                words.pop().unwrap()
+            }
         })
 }
 
@@ -57,8 +63,8 @@ where
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     many1(letter()).map(|e: String| match e.as_ref() {
-        "true" => Uni::Boolean(Boolean::True),
-        "false" => Uni::Boolean(Boolean::False),
+        "true" => return Uni::Boolean(Boolean::True),
+        "false" => return Uni::Boolean(Boolean::False),
         _ => Uni::Id(Id(e.into())),
     })
 }
@@ -114,11 +120,26 @@ mod test {
     use crate::expr::uni::*;
 
     #[test]
-    fn uni_test() {
+    fn boolean_test() {
         assert_eq!(
             uni().easy_parse(r#"true"#),
             Ok((Uni::Boolean(Boolean::True), ""))
         );
+    }
+
+    #[test]
+    fn number_test() {
         assert_eq!(uni().easy_parse(r#"123"#), Ok((Uni::Number(123), "")));
+    }
+
+    #[test]
+    fn field_test() {
+        assert_eq!(
+            uni().easy_parse(r#"abc.def"#),
+            Ok((
+                Uni::Field(vec![Id(String::from("abc")), Id(String::from("def")),]),
+                ""
+            ))
+        );
     }
 }
