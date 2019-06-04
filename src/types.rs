1f1f9c4b399ec158d37798bedc0ac5c5b2c8b9d1
rchaser53 <tayoshizawa29@gmail.com>
2019-06-04 23:56:26 +0900

    impl PartialEq for TypeKind

--- src/types.rs
+++ src/types.rs
@@ -3,13 +3,43 @@ use std::sync::Mutex;
 
 use crate::expr::uni::*;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub enum TypeKind {
     Int,
     String,
     Boolean,
     Undefined(Vec<Id>),
 }
+
+impl PartialEq for TypeKind {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            TypeKind::Int => {
+                if let TypeKind::Int = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TypeKind::String => {
+                if let TypeKind::String = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TypeKind::Boolean => {
+                if let TypeKind::Boolean = other {
+                    true
+                } else {
+                    false
+                }
+            }
+            TypeKind::Undefined(_) => false,
+        }
+    }
+}
+impl Eq for TypeKind {}
 
 lazy_static! {
     pub static ref TYPE_MAP: Mutex<HashMap<String, Uni>> = {
