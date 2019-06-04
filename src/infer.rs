a720867c939b1a184708e581e284c532f8150a3e
rchaser53 <tayoshizawa29@gmail.com>
2019-06-04 22:58:45 +0900

    add TypeResult

--- src/infer.rs
+++ src/infer.rs
@@ -1,17 +1,31 @@
 use std::collections::HashMap;
 
+use crate::expr::bin_op::*;
 use crate::expr::uni::*;
 use crate::expr::*;
 use crate::statement::*;
 use crate::types::*;
 
+type TypeMap = HashMap<Id, TypeResult>;
+
+#[derive(Debug, PartialEq)]
+pub enum TypeResult {
+    Resolved(TypeKind),
+    Uni(UniType),
+    Binary(Box<TypeResult>, BinOpKind, Box<TypeResult>),
+    Err(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct UniType(Id, TypeKind);
+
 pub fn infer(statements: Vec<Statement>) -> Result<(), String> {
     for statement in statements {
+        let mut type_map: TypeMap = HashMap::new();
         match statement {
             Statement::Let(id, exp, bodys) => {
-                let mut type_map: HashMap<Id, TypeKind> = HashMap::new();
                 let right_type = resolve_expr(exp);
-                type_map.insert(id.clone(), right_type?);
+                type_map.insert(id.clone(), right_type);
             }
             Statement::Expr(expr) => {
                 resolve_expr(expr);
@@ -22,22 +36,48 @@ pub fn infer(statements: Vec<Statement>) -> Result<(), String> {
     Ok(())
 }
 
-pub fn resolve_expr(exp: Expr) -> Result<TypeKind, String> {
+pub fn resolve_expr(exp: Expr) -> TypeResult {
     match exp {
-        Expr::Unary(uni) => Ok(resolve_type(uni)),
-        Expr::Binary(left, op, right) => unimplemented!(),
-        Expr::Call(ids, _) => Ok(TypeKind::Undefined(ids)),
+        Expr::Unary(uni) => resolve_type(uni),
+        Expr::Binary(left, op, right) => resolve_binary(*left, op, *right),
+        Expr::Call(ids, _) => unreachable!(),
         Expr::Fn(_, _, _) => unreachable!(),
     }
 }
 
-pub fn resolve_type(uni: Uni) -> TypeKind {
+pub fn resolve_binary(left: Expr, op: BinOpKind, right: Expr) -> TypeResult {
+    match (left, right) {
+        (Expr::Binary(l_left, l_op, l_right), Expr::Binary(r_left, r_op, r_right)) => {
+            let l_resolved = resolve_binary(*l_left, l_op, *l_right);
+            let r_resolved = resolve_binary(*r_left, r_op, *r_right);
+            TypeResult::Binary(Box::new(l_resolved), op, Box::new(r_resolved))
+        }
+        (Expr::Binary(l_left, l_op, l_right), Expr::Unary(right)) => {
+            let l_resolved = resolve_binary(*l_left, l_op, *l_right);
+            let r_resolved = resolve_type(right);
+            TypeResult::Binary(Box::new(l_resolved), op, Box::new(r_resolved))
+        }
+        (Expr::Unary(left), Expr::Binary(r_left, r_op, r_right)) => {
+            let l_resolved = resolve_type(left);
+            let r_resolved = resolve_binary(*r_left, r_op, *r_right);
+            TypeResult::Binary(Box::new(l_resolved), op, Box::new(r_resolved))
+        }
+        (Expr::Unary(left), Expr::Unary(right)) => {
+            let l_resolved = resolve_type(left);
+            let r_resolved = resolve_type(right);
+            TypeResult::Binary(Box::new(l_resolved), op, Box::new(r_resolved))
+        }
+        _ => unimplemented!(),
+    }
+}
+
+pub fn resolve_type(uni: Uni) -> TypeResult {
     match uni {
-        Uni::Id(id) => TypeKind::Undefined(vec![id]),
-        Uni::Field(fields) => TypeKind::Undefined(fields),
-        Uni::String(_) => TypeKind::String,
-        Uni::Number(_) => TypeKind::Int,
-        Uni::Boolean(_) => TypeKind::Boolean,
+        Uni::Id(id) => TypeResult::Uni(UniType(id.clone(), TypeKind::Undefined(vec![id]))),
+        Uni::String(_) => TypeResult::Resolved(TypeKind::String),
+        Uni::Number(_) => TypeResult::Resolved(TypeKind::Int),
+        Uni::Boolean(_) => TypeResult::Resolved(TypeKind::Boolean),
+        Uni::Field(fields) => unimplemented!(),
         Uni::Array(_) => unimplemented!(),
         Uni::HashMap(_) => unimplemented!(),
         Uni::Null => unimplemented!(),
